9235033b90b39fca938ea6fdd228b3eb3d6e92ed
Dennis Barrett <dbarrett@bscglobal.com>
2024-11-22 17:15:02 +0200

    Added cookiecutter template for a barebones Python package

---
+++ py-pkg/hooks/pre_gen_project.py
@@ -0,0 +1,18 @@
+import os
+import re
+import sys
+
+MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
+project_slug = "{{ cookiecutter.project_slug }}"
+
+if not re.match(MODULE_REGEX, project_slug):
+    print(f"ERROR: {project_slug} is not a valid Python module name!")
+    sys.exit(1)
+
+init_git = "{{ cookiecutter.init_git }}"
+if init_git:
+    os.system("git init")
+
+init_poetry = "{{ cookiecutter.init_poetry }}"
+if init_poetry:
+    os.system("poetry shell")
---
+++ py-pkg/{{cookiecutter.project_slug}}/README.md
@@ -0,0 +1,7 @@
+# {{cookiecutter.project_name}}
+
+{{cookiecutter.project_description}}
+
+## License
+
+{{cookiecutter.project_name}} is licensed under the {{cookiecutter.project_license}} license.
